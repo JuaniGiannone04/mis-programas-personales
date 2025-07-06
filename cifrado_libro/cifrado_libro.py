5f6c7cae95375fefa03e5b34bb457a64efd7a56d
JuaniGiannone <jgiannone@fi.uba.ar>
2025-07-06 18:19:15 -0300

    cifrado_libro.py

---
+++ cifrado_libro/cifrado_libro.py
@@ -0,0 +1,28 @@
+"Cifrado por libro (con letras)"
+
+import re
+
+
+def cifrado_libro(llave, mensaje):
+    """La funcion recibe una llave (un texto) y un texto
+    que se devolvera cifrado"""
+    cifrado = []
+    llaves = re.split(r"[ .,]+", llave)
+    mensajes = mensaje.split(" ")
+    for palabra in mensajes:
+        if palabra not in llaves:
+            raise ValueError("Mensaje no presente en la llave")
+        cifrado.append(llaves.index(palabra))
+    return cifrado
+
+
+def main():
+    "Funcion main de ejemplo"
+    llave_ej = "La casa oscura. El perro ladraba. El viento movia cortinas."
+    cifrado_ej = "perro oscura"
+    lista = cifrado_libro(llave_ej, cifrado_ej)
+    print(lista)
+
+
+if __name__ == "__main__":
+    main()
